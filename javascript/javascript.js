1fe208dcc6d0d011999b97a7b68f8cadf2a6ce97
juberaustad <119779839+juberaustad@users.noreply.github.com>
2023-05-11 15:56:47 +0530

    Javascript

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,42 +1,50 @@
-let fnameNode=document.getElementById('fullname');
+let fullnameNode=document.getElementById('fullname');
 let errorNode1=document.getElementById('error1');
 function validate1(){
     errorNode1.textContent="";
-    let fname=fnameNode.value;
-    let fnamePattern= new RegExp("^[A-Za-z]*$");
-    fnameNode.style.border="2px red solid";
+    let fname=fullnameNode.value;
+    let fullnamePattern= new RegExp("^[A-Za-z]*$");
+    fullnameNode.style.border="2px red solid";
     if(fname==""){
         errorNode1.textContent="Name is required";
+        return false;
     }
     else if(fname.length<5){
         errorNode1.textContent="Name must contain atleast 5 characters";
+        return false;
     }
-    else if(fnamePattern.test(fname)==false){
+    else if(fullnamePattern.test(fname)==false){
         errorNode1.textContent="Name must have only alphabets";
+        return false;
     }
     else{
-        fnameNode.style.border="2px green solid";
+        fullnameNode.style.border="2px green solid";
+        return true;
     }
 }
 
 
-let eNode=document.getElementById('email1');
+let emailNode=document.getElementById('email1');
 let errorNode2=document.getElementById('error2');
 function validate2(){
     errorNode2.textContent="";
-    let email=eNode.value;
-    eNode.style.border="2px red solid";
+    let email=emailNode.value;
+    emailNode.style.border="2px red solid";
     if(email==""){
         errorNode2.textContent="Email is required";
+        return false;
     }
     else if(!email.includes('@')){
         errorNode2.textContent="Please ente valid Email Id";
+        return false;
     }
     else if(email.endsWith('@')){
         errorNode2.textContent="Please enter valid Email Id";
+        return false;
     }
     else{
-        eNode.style.border="2px green solid";
+        emailNode.style.border="2px green solid";
+        return true;
     }
 }
 
@@ -48,11 +56,31 @@ function validate3(){
     textNode.style.border="2px red solid";
     if(texta==""){
         errorNode3.textContent="Massage is required";
+        return false;
     }
     else if(texta.length<20){
         errorNode3.textContent="Massage must contain atleast 20 characters";
+        return false;
     }
     else{
         textNode.style.border="2px green solid";
+        return true;
     }
+}
+
+// Submit Button JS
+
+let nodeArray=[fullnameNode,emailNode,textNode];
+function validateForm(){
+    let flag1=validate1();
+    let flag2=validate2();
+    let flag3=validate3();
+    let flagArray=[flag1,flag2,flag3];
+    for(let i=0;i<flagArray.length;i++){
+            if(flagArray[i]==false){
+                nodeArray[i].focus();
+                break;
+            }
+        }
+    return (flag1 && flag2  && flag3)
 }
